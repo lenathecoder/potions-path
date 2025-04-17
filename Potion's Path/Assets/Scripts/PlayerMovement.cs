7f66912eb8641897f10c2daeb3addae5c18de576
lenathecoder <lena.m.larson@gmail.com>
2025-04-17 13:50:17 -0700

    Removed heads

--- Potion's Path/Assets/Scripts/PlayerMovement.cs
+++ Potion's Path/Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-<<<<<<< HEAD
     private float speed = 10;
-=======
-    private float speed=10;
->>>>>>> 2fc833ac59b6ba69f4c9da737a94c150a2eeded0
+
     private Rigidbody2D body;
     private bool grounded;
 
@@ -17,12 +14,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-<<<<<<< HEAD
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal")*speed, body.linearVelocity.y);
-
-        if(Input.GetKey(KeyCode.Space))
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
-=======
         float horizontalInput = Input.GetAxis("Horizontal");
         body.linearVelocity = new Vector2(horizontalInput*speed, body.linearVelocity.y);
 
@@ -40,6 +31,5 @@ public class PlayerMovement : MonoBehaviour
     {
         body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
         grounded = false;
->>>>>>> 2fc833ac59b6ba69f4c9da737a94c150a2eeded0
     }
 }

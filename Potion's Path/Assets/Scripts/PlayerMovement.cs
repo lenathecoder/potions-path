b2add76c38c8770fae90bab58eb3f4baec270a47
lenathecoder <lena.m.larson@gmail.com>
2025-04-23 12:38:28 -0700

    Fixed jump & horizontal mvmt!

--- Potion's Path/Assets/Scripts/PlayerMovement.cs
+++ Potion's Path/Assets/Scripts/PlayerMovement.cs
@@ -2,21 +2,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [SerializeField] private LayerMask groundLayer;
+    private int jumpPower = 20;
     private float speed = 10;
     private Rigidbody2D body;
     private bool grounded;
     private Animator anim;
+    private BoxCollider2D boxCollider;
+    private float horizontalInput;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
         body.linearVelocity = new Vector2(horizontalInput*speed, body.linearVelocity.y);
 
         //Flip player when moving left to right
@@ -25,26 +29,30 @@ public class PlayerMovement : MonoBehaviour
         else if(horizontalInput<-.01f)
             transform.localScale = new Vector2(-1, 1);
 
-        if(Input.GetKey(KeyCode.Space) && grounded)
-            print (grounded);
-            Jump();
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", isGrounded());
 
-        //Set run bool
-        anim.SetBool("run", horizontalInput !=0 );
+        if(Input.GetKey(KeyCode.Space))
+            Jump();
     }
 
     private void Jump()
     {
-        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
-        grounded = false;
+        if(isGrounded())
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpPower);
+            grounded = false;
+        }
     }
     
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.tag == "Ground")
             grounded = true;
     }
-
-    private void RaycastHit2D Physics2D.BoxCast(Vector 2 origin, Vector 2 size, float angle, Vector2 direction, float distance){
-        
+    
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, .1f, groundLayer);
+        return raycastHit.collider != null;
     }
 }

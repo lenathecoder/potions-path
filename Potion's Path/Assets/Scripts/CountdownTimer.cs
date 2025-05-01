3a638a896a41ee26db1aeec2e89ababfa9929aee
lenathecoder <lena.m.larson@gmail.com>
2025-05-01 14:04:01 -0700

    Countdown for cutscene!

--- Potion's Path/Assets/Scripts/CountdownTimer.cs
+++ Potion's Path/Assets/Scripts/CountdownTimer.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CountdownTimer : MonoBehaviour
 {
-    public string levelToLoad;
-    private float timer = 10f;
+    private float timeLeft = 10f;
 
     void Update()
     {
-        timer -= timer.deltaTime;
-        if(timer <= 0)
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
         {
-            Application.LoadLevel(levelToLoad);
+            SceneManager.LoadScene("LevelOne");
         }
     }
 }

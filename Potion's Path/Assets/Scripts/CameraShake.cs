6823d6c60cf91ce6a7728e13cf28e4c04dfadc75
lenathecoder <lena.m.larson@gmail.com>
2025-05-02 14:47:54 -0700

    Camera shake changes

--- Potion's Path/Assets/Scripts/CameraShake.cs
+++ Potion's Path/Assets/Scripts/CameraShake.cs
@@ -4,25 +4,32 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     Vector2 camInitialPos;
-    private float shakeMagnitude = .05f;
-    private float shakeTime = .5f;
+    public float shakeMagnitude = .05f;
+    public float shakeTime = .5f;
+    public Camera mainCamera;
+
     public void Start()
     {
         camInitialPos = mainCamera.transform.position;
-        InvokeRepeating(Shake(), 0f, .005f);
-        StopShake();
+        InvokeRepeating("Shake", 0f, .005f);
+        Invoke("StopShake", shakeTime);
     }
 
-    // Update is called once per frame
     public void Shake()
     {
         float cameraShakingOffsetX = Random.value * shakeMagnitude * 2 - shakeMagnitude;
         float cameraShakingOffsetY = Random.value * shakeMagnitude * 2 - shakeMagnitude;
-        Vector2
+        Vector2 camPosition = mainCamera.transform.position;
+
+        camPosition += cameraShakingOffsetX;
+        camPosition += cameraShakingOffsetY;
+
+        mainCamera.transform.position = camPosition;
     }
 
     public void StopShake()
     {
-
+        CancelInvoke("Shake");
+        mainCamera.transform.position = camInitialPos;
     }
 }

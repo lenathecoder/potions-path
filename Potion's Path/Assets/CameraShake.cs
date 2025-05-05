a55849db1e7762afb3fac491defdb387508dac87
lenathecoder <lena.m.larson@gmail.com>
2025-05-05 14:43:55 -0700

    Camerashake

--- Potion's Path/Assets/CameraShake.cs
+++ Potion's Path/Assets/CameraShake.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using System.Collections;
 
 public class CameraShake : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public CameraShake cameraShake;
     void Update()
     {
-        
+        StartCoroutine(.15f, .4f);
+    }
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector2 originalPos = transform.localPosition;
+
+        float elapsed = 0f;
+
+        while(elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector2(x, y);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+
+            transform.localPosition = originalPos;
+        }
     }
 }
